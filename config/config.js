aa25f4aa90cebb7ad7ab1574762a73519f3d1cfe
nicolascoco85 <nicolascoco85@yahoo.com.ar>
2021-11-02 18:55:13 -0300

    se agrega un campo nuevo para token jwt

--- config/config.js
+++ config/config.js
@@ -5,6 +5,7 @@ module.exports = {
         useUnifiedTopology: true,
     },
     TOKEN_WEATHER: process.env.TOKEN_WEATHER ,
-    TOKEN_SECRET: process.env.TOKEN_SECRET || "meetup-secret-token-value",
-    FRONT_END_NAME: process.env.FRONT_END_NAME || "meetup-web-front-end",
+    TOKEN_SECRET: process.env.TOKEN_SECRET || "patena-secret-token-value",
+    FRONT_END_NAME: process.env.FRONT_END_NAME || "patena-web-front-end",
+    TOKEN_SECRET_JWT: process.env.TOKEN_SECRET_JWT ,
 }
