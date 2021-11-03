ee4f6ed5b84f5aafc4e6f6b3e6421a2f64e62942
nicolascoco85 <nicolascoco85@yahoo.com.ar>
2021-11-02 22:20:07 -0300

    debugger el server para deployar

--- services/validationService.js
+++ services/validationService.js
@@ -3,7 +3,7 @@ const moment = require('moment');
 
 function isValidDate(date) {
     const fecha = moment(date, 'YYYY-MM-DD');
-    const fechaInicial = moment(new Date(),'YYYY-MM-DD').add(-1,'days');
+    const fechaInicial = moment(new Date(),'YYYY-MM-DD').add(-2,'days');
 
     const fechaInicialFormated = fechaInicial.toISOString().substring(0, 10);
     const fechaLimite = moment(fechaInicialFormated, "YYYY-MM-DD").add(15, 'days');
