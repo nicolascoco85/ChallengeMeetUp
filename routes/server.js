747440b3253e6aa5abfaecfa0c2e376ae8c76ec0
nicolascoco85 <nicolascoco85@yahoo.com.ar>
2021-11-02 23:06:50 -0300

    se agrega el endpoint para crear la meetup

--- routes/server.js
+++ routes/server.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const DB = require('../models/database');
 const weatherService = require('../services/weatherService');
 const validationService = require('../services/validationService');
+const meetUpService = require('../services/meetUpService');
 
 app.use(bodyParser.urlencoded({
     extended: true
@@ -34,6 +35,29 @@ app.get('/clima/', async (request, response) => {
 
 
 });
+
+app.post('/meetup', async(req, res) => {
+    console.log(req.body.date)
+    console.log(req.body.name)
+    const fecha = req.body.date.toString()
+    const name = req.body.name.toString()
+
+    if (validationService.isFutureDay(fecha)) {
+        try {
+            await meetUpService.create(name,fecha);
+            res.json({state:"Created"})
+        } catch (error) {
+            res.status(400).send({
+                message: "Out range date value " + fecha,
+            });
+        }
+    } else {
+        return res.status(400).send({
+            message: "Out range date value " + fecha,
+        });
+    }
+});
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
